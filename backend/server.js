cc835aea3686f0de208143d70bbde0c22109588c
Daniel Rose <daniel.rose.1928@gmail.com>
2026-04-02 13:23:14 -0400

    Refactor server to use noteRoutes for API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,10 @@
 import express from "express";
+import noteRoutes from "./routes/notesRoutes";
+import router from "./routes/notesRoutes";
 
 const APP = express();
 const PORT = 5001;
-
-APP.get("/api/notes", (req, res) => {
-    res.status(200).send("You got 5 notes");
-})
-
-APP.post("api/notes", (req, res) => {
-    res.status(201).json({message: "Note created successfully."});
-})
-
-APP.put("api/notes/:id", (req, res) => {
-    res.status(200).json({ message: `Note updated successfully` });
-})
-
-APP.delete("api/notes/:id", (req, res) => {
-    res.status(200).json({ message: `Note updated successfully` });
-})
+APP.use("/api/notes", noteRoutes);
 
 APP.listen(PORT, () => {
     console.log(`Sever started on port: ${PORT}`)
