344e20dcb3cd5497d2a16e093e8a9feb4f6b5c52
Daniel Rose <daniel.rose.1928@gmail.com>
2026-04-04 20:33:36 -0400

    feat: add initial setup for notes API with MongoDB integration
    - Created database connection module using mongoose
    - Implemented notes controller with CRUD operations
    - Set up routes for notes API
    - Configured server to use notes routes and connect to MongoDB
    - Added whatwg-url package for URL handling

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv';
+
+const APP = express();
+const PORT = process.env.PORT || 5001;
+
+dotenv.config();
+
+connectDB();
+
+APP.use("/api/notes", notesRoutes);
+
+APP.listen(PORT, () => {
+    console.log(`Sever started on port: ${PORT}`)
+})
+
+// mongodb+srv://danielrose1928_db_user:aYJvFa3KtcawXQDV@cluster0.5tagksq.mongodb.net/?appName=Cluster0
